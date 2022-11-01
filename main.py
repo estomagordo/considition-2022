1b99bf12bad9626f56dcf9f4bd5f686f43c1f042
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2022-11-01 09:42:30 +0100

    from upstream

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ with open('secret') as f:
 
 # The different map names can be found on considition.com/rules
 # TODO: You map choice here. Unless changed, the map "training1" will be selected.
-map_name = "Suburbia"
+map_name = "training1"
 # TODO: You bag type choice here. Unless changed, the bag type 1 will be selected.
 bag_type = 1
 
 def main():
 	print("Starting game...")
 	response = api.mapInfo(api_key, map_name)
-	days = 31 if map_name == "training1" or map_name == "training2" else 365
-	bagType = 1
+
+	days = 31 if map_name == "Suburbia" or map_name == "Fancyville" else 365
 
 	solver = Solver(game_info=response)
-	solution = solver.Solve(bagType, days)
+	solution = solver.Solve(bag_type, days)
 
 	submit_game_response = api.submit_game(api_key, map_name, solution)
 	print(submit_game_response)
+
 if __name__ == "__main__":
-    main()
+	main()
--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import json
+
 class Solution:
     def __init__(self, recycleRefundChoice, bagPrice, refundAmount, bagType):
         self.recycleRefundChoice = recycleRefundChoice
@@ -5,3 +7,9 @@ class Solution:
         self.refundAmount = refundAmount
         self.bagType = bagType
         self.orders = []
+
+    def addOrder(self, int):
+        self.orders.append(int)
+
+    def toJSON(self):
+        return json.loads(json.dumps(self, default=lambda o: o.__dict__))
